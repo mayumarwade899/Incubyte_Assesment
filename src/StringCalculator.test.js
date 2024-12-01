b92bcd070055f1cc27739628862fdc745a7380b7
Mayur Marwade <mayumarwade899@gmail.com>
2024-12-01 13:49:42 +0530

    added support to handle new lines between numbers

--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -16,3 +16,7 @@ test("return the sum of two numbers", () => {
 test("handles multiple numbers", () => {
     expect(add("1,2,3,4")).toBe(10)
 })
+
+test("handles new lines between numbers", () => {
+    expect(add("1\n2,3")).toBe(6)
+})
