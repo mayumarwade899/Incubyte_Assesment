868abf0f30884e4a59ef685c3ef26fdfa65f57ea
Mayur Marwade <mayumarwade899@gmail.com>
2024-12-01 13:56:52 +0530

    added support for different delimiters

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,8 +1,15 @@
 function add(numbers) {
   if (numbers === "") return 0;
 
+  let delimiter = /,|\n/;
+  if(numbers.startsWith("//")) {
+    const parts = numbers.match(/^\/\/(.+)\n(.+)/)
+    delimiter = new RegExp(parts[1]);
+    numbers = parts[2];
+  } 
+
   return numbers
-    .split(/,|\n/)
+    .split(delimiter)
     .map(Number)
     .reduce((sum, num) => sum + num, 0);
 }
--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -20,3 +20,8 @@ test("handles multiple numbers", () => {
 test("handles new lines between numbers", () => {
     expect(add("1\n2,3")).toBe(6)
 })
+
+test("supoort different delimiters", () => {
+    expect(add("//;\n1;2;3")).toBe(6)
+})
+
